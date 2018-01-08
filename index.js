37f9adcdf5a5a7c703700d3cd193a4385be89a5b
huyvu05051996 <huyvu05051996@gmail.com>
2018-01-08 21:27:11 +0700

    Update 8/1
    Update folder src

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 import { AppRegistry } from 'react-native';
-import App from './Gongcha/App';
-import Shop from './Gongcha/screen/Cart/Shop'
-import Menu from './Gongcha/screen/Detail/TabMenu'
+import App from './src/App';
 AppRegistry.registerComponent('GoodFooder', () => App);
